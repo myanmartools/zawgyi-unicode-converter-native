b2c556ee866c37f947b97f0e764fb435f245c0bf
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-04 17:42:01 +0630

    Update about-modal.component.ts

--- src/app/about/about-modal.component.ts
+++ src/app/about/about-modal.component.ts
@@ -12,10 +12,9 @@ import { ModalController, ToastController } from '@ionic/angular';
 
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
-import { ConfigService } from '@dagonmetric/ng-config';
 import { LogService } from '@dagonmetric/ng-log';
 
-import { AppConfig, NavLinkItem } from '../shared';
+import { NavLinkItem, appSettings } from '../shared';
 
 /**
  * The app about modal component.
@@ -33,41 +32,35 @@ export class AboutModalComponent {
     };
 
     get appName(): string {
-        return this._appConfig.appName;
+        return appSettings.appName;
     }
 
     get appVersion(): string {
-        return this._appConfig.appVersion;
+        return appSettings.appVersion;
     }
 
     get navLinks(): NavLinkItem[] {
-        return this._appConfig.navLinks;
+        return appSettings.navLinks;
     }
-
-    private readonly _appConfig: AppConfig;
 
     constructor(
         private readonly _modalController: ModalController,
         private readonly _toastController: ToastController,
         private readonly _socialSharing: SocialSharing,
-        private readonly _logService: LogService,
-        configService: ConfigService
-    ) {
-        this._appConfig = configService.getValue<AppConfig>('app');
-    }
+        private readonly _logService: LogService
+    ) {}
 
     dismissModal(): void {
-        // tslint:disable-next-line: no-floating-promises
-        this._modalController.dismiss({
+        void this._modalController.dismiss({
             dismissed: true
         });
     }
 
     async showShareSheet(): Promise<void> {
-        this._appConfig.socialSharing = this._appConfig.socialSharing || {};
-        const socialSharingSubject = this._appConfig.socialSharing.subject;
-        const socialSharingLink = this._appConfig.socialSharing.linkUrl;
-        const socialSharingMessage = this._appConfig.socialSharing.message;
+        appSettings.socialSharing = appSettings.socialSharing || {};
+        const socialSharingSubject = appSettings.socialSharing.subject;
+        const socialSharingLink = appSettings.socialSharing.linkUrl;
+        const socialSharingMessage = appSettings.socialSharing.message;
 
         try {
             // tslint:disable-next-line: no-floating-promises
@@ -82,18 +75,18 @@ export class AboutModalComponent {
                 message: 'Thank you for sharing the app 😊.',
                 duration: 2000
             });
-            // tslint:disable-next-line: no-floating-promises
-            toast.present();
+
+            void toast.present();
 
             this._logService.trackEvent({
                 name: 'share',
                 properties: {
                     method: 'Social Sharing Native',
-                    app_version: this._appConfig.appVersion
+                    app_version: appSettings.appVersion
                 }
             });
         } catch (err) {
-            // tslint:disable-next-line: no-unsafe-any
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             const errMsg = err && err.message ? ` ${err.message}` : '';
             this._logService.error(`An error occurs when sharing via Web API.${errMsg}`);
         }
