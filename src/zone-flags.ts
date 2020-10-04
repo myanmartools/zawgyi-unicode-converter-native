acc2c21c64a25eab2c0905ee39f6a5b68143bb1c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-04 17:26:45 +0630

    Update zone-flags.ts

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -2,5 +2,6 @@
  * Prevents Angular change detection from
  * running with certain Web Component callbacks
  */
-// tslint:disable-next-line: no-any
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
 (window as any).__Zone_disable_customElements = true;
