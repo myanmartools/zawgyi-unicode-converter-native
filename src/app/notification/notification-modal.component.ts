3c260fdaccffeeb4b8a9c52e981bd7abde1b3b63
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-11 12:07:21 +0630

    Update notification component

--- src/app/notification/notification-modal.component.ts
+++ src/app/notification/notification-modal.component.ts
@@ -8,7 +8,13 @@
 
 import { Component, Input } from '@angular/core';
 
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
+
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+import { LogService } from '@dagonmetric/ng-log';
+
+import { appSettings } from '../shared';
 
 /**
  * The app notification modal component.
@@ -38,13 +44,55 @@ export class NotificationModalComponent {
     imageUrl: string;
 
     @Input()
-    isAd: boolean;
+    isAd: string;
 
-    constructor(private readonly _modalController: ModalController) {}
+    constructor(
+        private readonly _modalController: ModalController,
+        private readonly _toastController: ToastController,
+        private readonly _socialSharing: SocialSharing,
+        private readonly _logService: LogService
+    ) {}
 
     dismissModal(): void {
         void this._modalController.dismiss({
             dismissed: true
         });
+    }
+
+    async showShareSheet(): Promise<void> {
+        if (!this.link) {
+            return;
+        }
+
+        const socialSharingSubject = this.titleText || this.bodyText;
+        const socialSharingLink = this.link;
+        const socialSharingMessage = this.bodyText;
+
+        try {
+            await this._socialSharing.shareWithOptions({
+                message: socialSharingMessage,
+                subject: socialSharingSubject,
+                url: socialSharingLink
+                // appPackageName
+            });
+
+            const toast = await this._toastController.create({
+                message: 'မျှဝေတဲ့အတွက် ကျေးဇူးတင်ပါတယ်။',
+                duration: 5000,
+                cssClass: 'my-uni'
+            });
+
+            void toast.present();
+
+            this._logService.trackEvent({
+                name: this.isAd ? 'share_ad_noti' : 'share_ad',
+                properties: {
+                    method: 'share',
+                    app_version: appSettings.appVersion
+                }
+            });
+        } catch (err) {
+            this._logService.error(`An error occurs when sharing via Web API. ${err}`);
+        }
     }
 }
